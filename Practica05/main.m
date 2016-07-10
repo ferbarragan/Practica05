83c2d6632bae9dcf7453b8959490e53408ec5dce
ferbarragan <ferbarragan.developer@gmail.com>
2016-07-10 18:25:41 -0500

    Initial Commit

---
+++ Practica05/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practica05
+//
+//  Created by Christian Barragan on 10/07/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
